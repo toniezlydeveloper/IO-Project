b2bc23f74470c84f73786ad02a28b97d1ca44721
Adam Janikowski <adam.janikowski@dreamstormstudios.com>
2021-01-26 17:50:39 +0100

    Added IO layer system definition

---
+++ IO/RequestBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IO_Project.IO
+{
+    class RequestBuilder
+    {
+        private Request request = new Request();
+
+        public Request Build()
+        {
+            return request;
+        }
+
+        public RequestBuilder OfType(RequestType type)
+        {
+            request.Type = type;
+            return this;
+        }
+
+        public RequestBuilder WithPayload(object payload)
+        {
+            request.Payload = payload;
+            return this;
+        }
+
+        public RequestBuilder WithCallback(Action<object> callback)
+        {
+            request.Callback = callback;
+            return this;
+        }
+
+        public RequestBuilder WithFailCallback(Action failCallback)
+        {
+            request.FailCallback = failCallback;
+            return this;
+        }
+    }
+}
