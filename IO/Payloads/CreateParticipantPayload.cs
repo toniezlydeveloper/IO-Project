63cecfa5d7dc6582bc6ac86c49267f6fd83f126b
Adam Janikowski <adam.janikowski@dreamstormstudios.com>
2021-01-26 18:43:53 +0100

    Create CreateParticipantPayload.cs

---
+++ IO/Payloads/CreateParticipantPayload.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IO_Project.IO.Payloads
+{
+    class CreateParticipantPayload
+    {
+        public string Name { get; }
+        public string Surname { get; }
+
+        public CreateParticipantPayload(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+    }
+}
